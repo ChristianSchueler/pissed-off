561f614b4800e971408024591e503620a8f9cd07
ChristianSchueler <hello@christianschueler.at>
2025-11-23 18:28:05 +0100

    add timeout during dispensing

--- esp-32-s3/main/main.c
+++ esp-32-s3/main/main.c
@@ -8,6 +8,7 @@
 #include "peristaltic-pump.h"
 #include "coin-acceptor.h"
 #include "load-cell.h"
+#include <esp_timer.h>
 
 enum State {
   INSERT_COIN,
@@ -15,17 +16,17 @@ enum State {
   TAKE_CUP
 };
 
-// how much we intentionally slow down the main loop
-#define MAIN_LOOP_DELAY_MS 100
-
-#define COCKTAIL_DONATION_CENTS 300
-#define CUP_WEIGHT_MIN_GRAMS 3
-#define CUP_WEIGHT_MAX_GRAMS 20
-#define COCKTAIL_SIZE_ML 100
+#define MAIN_LOOP_DELAY_MS 100                // how much we intentionally slow down the main loop
+#define COCKTAIL_DONATION_CENTS 300           // how much to donate at least
+#define COCKTAIL_SIZE_ML 100                  // cocktail size for donation
+#define CUP_WEIGHT_MIN_GRAMS 3                // minimum empty cup weight, used to identify placement of a cup
+#define CUP_WEIGHT_MAX_GRAMS 20               // maximum empty cup weight
+#define DISPENSING_TIMEOUT_MS 1000*20         // after dispensing time out we stop; most probably empty supply
 
 enum State state = INSERT_COIN;
-int drink_dispensed_ml;
-float initial_weight_grams;
+
+float initial_weight_grams;                   // remember weight when starting to fill the cup. most probably very small
+uint64_t dispensing_started_time_ms;          // when we started to dispense; used for timeout
 
 void app_main(void)
 {
@@ -38,6 +39,7 @@ void app_main(void)
         coin_acceptor_loop();
         load_cell_loop();
 
+        // keep track of donation and weight
         float weight = load_cell_get_last_load_grams();
         int coins = coin_acceptor_get_amount_cents();
 
@@ -46,18 +48,19 @@ void app_main(void)
             if (coins >= COCKTAIL_DONATION_CENTS && 
                 weight >= CUP_WEIGHT_MIN_GRAMS && weight <= CUP_WEIGHT_MAX_GRAMS) {
                   printf("pissed off: coins donated and cup placed -> dispensing");
-                  drink_dispensed_ml = 0;
                   initial_weight_grams = weight;
+                  dispensing_started_time_ms = esp_timer_get_time();
                   set_peristaltic_pump_on();
                   state = DISPENSING;
                 }
             break;
-          
-            // TODO implement TIMEOUT
+
           case DISPENSING:
-            drink_dispensed_ml = (load_cell_get_last_load_grams() - initial_weight_grams);    // assuming ml == grams
-            if (drink_dispensed_ml >= COCKTAIL_SIZE_ML) {
-              printf("pissed off: cup filled -> take cup");
+            int drink_dispensed_ml = load_cell_get_last_load_grams() - initial_weight_grams;    // assuming ml == grams
+            uint64_t duration_ms = esp_timer_get_time() - dispensing_started_time_ms;
+            if (drink_dispensed_ml >= COCKTAIL_SIZE_ML || duration_ms > DISPENSING_TIMEOUT_MS) {
+              if (duration_ms > DISPENSING_TIMEOUT_MS) printf("pissed off: ERROR ran out of supply");
+              else printf("pissed off: cup filled -> take cup");
               set_peristaltic_pump_off();
               state = TAKE_CUP;
             }
@@ -71,10 +74,8 @@ void app_main(void)
               state = INSERT_COIN;
             }
             break;
-
-          default:
-            break;
         }
+
         vTaskDelay(MAIN_LOOP_DELAY_MS / portTICK_PERIOD_MS);
     }
 }
